a09a1f95cf2d6061cd1c8c445dbed486ab25c68d
lecoutre <lecoutre@cril.fr>
2020-10-15 13:50:39 +0200

    First phase for x%y = z

--- src/main/java/constraints/hard/primitive/CtrPrimitiveTernary.java
+++ src/main/java/constraints/hard/primitive/CtrPrimitiveTernary.java
@@ -9,12 +9,14 @@
 package constraints.hard.primitive;
 
 import org.xcsp.common.Types.TypeConditionOperatorRel;
+import org.xcsp.common.Types.TypeOperatorRel;
 import org.xcsp.modeler.entities.CtrEntities.CtrAlone;
 
 import constraints.hard.global.SumWeighted;
 import interfaces.TagGACGuaranteed;
 import problem.Problem;
 import utility.Kit;
+import utility.exceptions.MissingImplementationException;
 import variables.Variable;
 import variables.domains.Domain;
 
@@ -51,6 +53,8 @@ public abstract class CtrPrimitiveTernary extends CtrPrimitive { // implements T
 
 			int[] resx, resy, resz; // residues for values in the domains of x, y and z
 
+			boolean multidirectional = false; // hard coding
+
 			@Override
 			public final boolean checkValues(int[] t) {
 				return t[0] + t[1] == t[2];
@@ -68,6 +72,8 @@ public abstract class CtrPrimitiveTernary extends CtrPrimitive { // implements T
 				return false;
 			}
 
+			// using labels and continue label is very inefficient. is that sure?
+
 			@Override
 			public boolean runPropagator(Variable dummy) {
 				for (int a = domx.first(); a != -1; a = domx.next(a)) {
@@ -81,6 +87,10 @@ public abstract class CtrPrimitiveTernary extends CtrPrimitive { // implements T
 							break;
 						if (domz.isPresentValue(w)) {
 							resx[a] = b;
+							if (multidirectional) {
+								resy[b] = a;
+								resz[domz.toIdx(w)] = a;
+							}
 							found = true;
 							break;
 						}
@@ -99,6 +109,8 @@ public abstract class CtrPrimitiveTernary extends CtrPrimitive { // implements T
 							break;
 						if (domz.isPresentValue(w)) {
 							resy[b] = a;
+							if (multidirectional)
+								resz[domz.toIdx(w)] = a;
 							found = true;
 							break;
 						}
@@ -120,6 +132,113 @@ public abstract class CtrPrimitiveTernary extends CtrPrimitive { // implements T
 							found = true;
 							break;
 						}
+					}
+					if (!found && domz.remove(c) == false)
+						return false;
+				}
+				return true;
+			}
+		}
+	}
+
+	public static abstract class CtrPrimitiveTernaryMod extends CtrPrimitiveTernary {
+
+		public static CtrAlone buildFrom(Problem pb, Variable x, Variable y, TypeConditionOperatorRel op, Variable z) {
+			if (op == TypeConditionOperatorRel.EQ)
+				return pb.addCtr(new TernaryModEQ(pb, x, y, z));
+			throw new MissingImplementationException();
+		}
+
+		public CtrPrimitiveTernaryMod(Problem pb, Variable x, Variable y, Variable z) {
+			super(pb, x, y, z);
+		}
+
+		public static final class TernaryModEQ extends CtrPrimitiveTernary implements TagGACGuaranteed {
+
+			int[] resx, resy, resz1, resz2; // residues for values in the domains of x, y and z
+
+			@Override
+			public final boolean checkValues(int[] t) {
+				return t[0] % t[1] == t[2];
+			}
+
+			public TernaryModEQ(Problem pb, Variable x, Variable y, Variable z) {
+				super(pb, x, y, z);
+				Kit.control(x.dom.firstValue() >= 0 && y.dom.firstValue() > 0 && z.dom.firstValue() >= 0);
+				this.resx = Kit.repeat(-1, domx.initSize());
+				this.resy = Kit.repeat(-1, domy.initSize());
+				this.resz1 = Kit.repeat(-1, domz.initSize());
+				this.resz2 = Kit.repeat(-1, domz.initSize());
+			}
+
+			@Override
+			public Boolean isSymmetric() {
+				return false;
+			}
+
+			@Override
+			public boolean runPropagator(Variable dummy) {
+				for (int a = domx.first(); a != -1; a = domx.next(a)) {
+					int va = domx.toVal(a);
+					if (va < domy.lastValue() && domz.isPresentValue(va)) // remainder is then necessarily va
+						continue;
+					if (resx[a] != -1 && domy.isPresent(resx[a]) && domz.isPresentValue(va % domy.toVal(resx[a])))
+						continue;
+					boolean found = false;
+					for (int b = domy.first(); b != -1; b = domy.next(b)) {
+						int vb = domy.toVal(b);
+						if (vb > va) // means that the remainder with remaining values of y lead to va (and this has been considered earlier)
+							break;
+						if (domz.isPresentValue(va % vb)) {
+							resx[a] = b;
+							found = true;
+							break;
+						}
+					}
+					if (!found && domx.remove(a) == false)
+						return false;
+				}
+				// y todo
+
+				// z
+				if (domz.removeValues(TypeOperatorRel.GE, domy.lastValue()) == false) // because remainder is less than the denominator
+					return false;
+				for (int c = domz.first(); c != -1; c = domz.next(c)) {
+					int vc = domz.toVal(c);
+					if (resz1[c] != -1 && domx.isPresent(resz1[c]) && domy.isPresent(resz2[c]))
+						continue;
+					boolean found = false;
+					for (int b = domy.last(); b != -1; b = domy.prev(b)) {
+						int vb = domy.toVal(b);
+						if (vc > vb)
+							break;
+						int nMultiples = domx.lastValue() / b;
+						if (domx.size() <= nMultiples) {
+							for (int a = domx.first(); a != -1; a = domx.next(a)) {
+								if (domx.toVal(a) % vb == vc) {
+									resz1[c] = a;
+									resz2[c] = b;
+									found = true;
+									break;
+								}
+							}
+						} else {
+							int multiple = vc;
+							while (true) {
+								if (multiple > domx.lastValue())
+									break;
+								if (domx.isPresentValue(multiple)) {
+									resz1[c] = domx.toIdx(multiple);
+									resz2[c] = b;
+									found = true;
+									break;
+								}
+								multiple += vb;
+							}
+						}
+						if (found)
+							break;
+
 					}
 					if (!found && domz.remove(c) == false)
 						return false;
